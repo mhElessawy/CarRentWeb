96e35ea83a98ab5f74b25c4b2ce89012470209fd
elessawy <melessawyh@gmail.com>
2026-05-01 17:47:11 +0300

    1-5-2026 v1

---
+++ Migrations/20260430195519_Deff.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarRentWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class Deff : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DomainRenewalDate",
+                table: "DeffInformation",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "MessageRenewalDate",
+                table: "DeffInformation",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SslRenewalDate",
+                table: "DeffInformation",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "VpsRenewalDate",
+                table: "DeffInformation",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DomainRenewalDate",
+                table: "DeffInformation");
+
+            migrationBuilder.DropColumn(
+                name: "MessageRenewalDate",
+                table: "DeffInformation");
+
+            migrationBuilder.DropColumn(
+                name: "SslRenewalDate",
+                table: "DeffInformation");
+
+            migrationBuilder.DropColumn(
+                name: "VpsRenewalDate",
+                table: "DeffInformation");
+        }
+    }
+}
